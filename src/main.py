c07a2998f9393984c101f0cb5e7764f1768d555b
wbuz24 <wbuziak@vols.utk.edu>
2023-03-17 14:45:12 -0400

    excel update

--- src/main.py
+++ src/main.py
@@ -10,14 +10,7 @@ def csvread():
     global df
     filePath = entry.get() # grabs filepath from input box
     df = pd.read_excel(filePath)
-    z = df['Time']
-    x = df['Wh/Kg']
-    y = df['Wh/L']
 
-    
-    for i in range(28):
-       timeEntries[i].delete(0, 'end')
-       timeEntries[i].insert(0, [z[i]])
 
 
 # initialize a window
@@ -29,12 +22,6 @@ window.title("Li-Ion Visualizer")
 entry = tk.Entry(window, width = 20)
 entry.grid(row = 2, column = 15, padx = (20, 0), pady = 1)
 entry.insert(0, os.getcwd() + '/include/Li_ion_battery_figure.xlsx')
-
-timeEntries = []
-
-for i in range(28):
-    timeEntries.append(tk.Entry(window, width = 5))
-    timeEntries[i].grid(row = i + 5, column = 2, padx = 10, pady = 1)
 
 # create an import button 
 import_button = tk.Button(window, text = "Import", command = csvread)
