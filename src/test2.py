9d8aaff6c5c3a947f86cf517325dc83a93e0e25b
wbuz24 <wbuziak@vols.utk.edu>
2023-07-17 10:23:42 -0400

    7/17/23

--- src/test2.py
+++ src/test2.py
@@ -2,11 +2,11 @@ import plotly.express as px
 import pandas as pd
 
 
-filePath = "/Users/thomasdonahue/Desktop/Classes/Research/Li-Ion-Visualizer/include/Li_ion_battery_figure.xlsx" 
+filePath = "./include/Li_ion_battery_figure.xlsx" 
     
 df = pd.read_excel(filePath)
 
 fig = px.scatter_3d(df, x='Year', y='Specific Energy (Wh/kg)', z='Specific Power (W/kg)',
               color='Color')
 fig.update_layout(showlegend=False)
-fig.write_html("/Users/thomasdonahue/Desktop/Classes/Research/Li-Ion-Visualizer/html/chart.html")
+fig.write_html("./html/chart1.html")
