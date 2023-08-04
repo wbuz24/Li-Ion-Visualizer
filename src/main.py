c27d147eb18c53ec22c5bf374924458ae8e2840b
ThomasDonahue01 <tdonahu3@vols.utk.edu>
2023-08-04 16:44:33 -0400

    Tom 06/04/23

--- src/main.py
+++ src/main.py
@@ -5,6 +5,7 @@ import pandas as pd
 filePath = "/Users/thomasdonahue/Desktop/Classes/Research/Li-Ion-Visualizer/include/Li_ion_battery_figure.xlsx" 
     
 df = pd.read_excel(filePath)
+print(df)
 
 fig = px.scatter_3d(df, x='Year', y='Specific Energy (Wh/kg)', z='Specific Power (W/kg)',
               color='Color', hover_data={'Year': True, 'Specific Energy (Wh/kg)': True, 'Specific Power (W/kg)': True, 'Color': False})
