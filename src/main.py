32d70f9830e083cfe73fb0a35dabfadbeee659cf
anirban22roy <aroy9@vols.utk.edu>
2023-09-06 20:51:58 -0400

    changes to legend and excel file made

--- src/main.py
+++ src/main.py
@@ -2,24 +2,13 @@ import plotly.express as px
 import pandas as pd
 
 
-filePath = "../Li-Ion-Visualizer/include/Li_ion_battery_figure.xlsx" 
+filePath = "../include/Li_ion_battery_figure.xlsx" 
     
 df = pd.read_excel(filePath)
 print(df)
 
-def update_point(trace, points, selector):
-    c = list(scatter.marker.color)
-    s = list(scatter.marker.size)
-    for i in points.point_inds:
-        c[i] = '#bae2be'
-        s[i] = 20
-        with f.batch_update():
-            scatter.marker.color = c
-            scatter.marker.size = s
-
-# scatter.on_click(update_point)
-
 fig = px.scatter_3d(df, x='Year', y='Specific Energy (Wh/kg)', z='Specific Power (W/kg)',
-              color='Color', hover_data={'Year': True, 'Specific Energy (Wh/kg)': True, 'Specific Power (W/kg)': True, 'Color': False})
+              color='Reference', hover_data={'Year': True, 'Specific Energy (Wh/kg)': True, 'Specific Power (W/kg)': True, 'Reference': True})
 fig.update_layout(showlegend=True)
-fig.write_html("../Li-Ion-Visualizer/html/chart.html")
+# fig.show()
+fig.write_html("../html/chart.html")
