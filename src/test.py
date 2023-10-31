3b75cb6279cdff28fd4107aeba232f6bb2615a8f
wbuz24 <wbuziak@vols.utk.edu>
2023-10-31 17:36:36 -0400

    iframe code maybe

--- src/test.py
+++ src/test.py
@@ -6,19 +6,21 @@ import io
 
 app = Dash(__name__)
 
-buffer = io.StringIO("Ragone.html")
+buffer = io.StringIO("ragone_graph.html")
 
 df = pd.read_excel("../Li-Ion-Visualizer/include/Li_ion_battery_figure.xlsx") # replace with your own data source
 fig = px.scatter_3d(df, x='Year', y='Specific Energy (Wh/kg)', z='Specific Power (W/kg)',
               color='Color', hover_data={'Year': True, 'Specific Energy (Wh/kg)': True, 'Specific Power (W/kg)': True, 'Color': False})
+fig.update_layout(showlegend=True)
 fig.write_html(buffer)
 
 html_bytes = buffer.getvalue().encode()
 encoded = b64encode(html_bytes).decode()
+with open('encoded.txt', 'w') as f:
+    f.write(encoded)
 
 app.layout = html.Div([
-    html.H4('Simple plot export options'),
-    html.P("↓↓↓ try downloading the plot as PNG ↓↓↓", style={"text-align": "right", "font-weight": "bold"}),
+    html.H4('Li-Ion Ragone Plot - University of Tennessee, Knoxville'),
     dcc.Graph(id="graph", figure=fig),
     html.A(
         html.Button("Download as HTML"), 
